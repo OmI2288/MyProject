16d6f60b59e0b1a87fc84d040357106760bd4d90
Admin <Admin@LAPTOP-JLT3GSO9>
2022-05-03 18:27:10 +0530

    if else ladder

--- projectOne/src/firstProject/GreaterIn3.java
+++ projectOne/src/firstProject/GreaterIn3.java
@@ -3,22 +3,20 @@ package firstProject;
 public class GreaterIn3 {
 	public static void main(String[] args) {
 
+		// NESTED IF - CONDITIONAL STATEMENT
+
 		int a = 30;
 		int b = 40;
 		int c = 50;
 
-		if (a > b) 
-		{
-			if (a > c)
-			{
+		if (a > b) {
+			if (a > c) {
 				System.out.println("a is GREATER");
 			} else {
 				System.out.println("c is GREATER");
 			}
-		} else
-		{
-			if (b > a) 
-			{
+		} else {
+			if (b > a) {
 				System.out.println(" b is Greater");
 			} else {
 				System.out.println("c is greater");
