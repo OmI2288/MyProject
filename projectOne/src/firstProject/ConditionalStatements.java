b7ba5965782ae0c27ebac2dd970cb264c50440d3
Admin <Admin@LAPTOP-JLT3GSO9>
2022-05-03 17:41:34 +0530

    Second Commit Conditional statements

---
+++ projectOne/src/firstProject/ConditionalStatements.java
@@ -0,0 +1,15 @@
+package firstProject;
+
+public class ConditionalStatements {
+	public static void main(String[] args) {
+
+		int number = 10;
+
+		if (number < 5) {
+			System.out.println("number is less than 5");
+		} else {
+			System.out.println("Number is Greater than 5");
+		}
+	}
+
+}
