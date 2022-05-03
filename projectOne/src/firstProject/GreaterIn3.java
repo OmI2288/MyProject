1d169e0900fb8cf62eb2ec53d91a35224c932aa7
Admin <Admin@LAPTOP-JLT3GSO9>
2022-05-03 18:17:47 +0530

    commited if elese programm

---
+++ projectOne/src/firstProject/GreaterIn3.java
@@ -0,0 +1,30 @@
+package firstProject;
+
+public class GreaterIn3 {
+	public static void main(String[] args) {
+
+		int a = 30;
+		int b = 40;
+		int c = 50;
+
+		if (a > b) 
+		{
+			if (a > c)
+			{
+				System.out.println("a is GREATER");
+			} else {
+				System.out.println("c is GREATER");
+			}
+		} else
+		{
+			if (b > a) 
+			{
+				System.out.println(" b is Greater");
+			} else {
+				System.out.println("c is greater");
+			}
+
+		}
+
+	}
+}
